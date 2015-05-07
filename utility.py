e11568aa4c1403453f41c702a132f4bc8332f33b
Syaoming <syaomingl@gmail.com>
2015-05-07 15:26:58 +0800

    testint minimal Tf sets

--- utility.py
+++ utility.py
@@ -29,18 +29,29 @@ def readModel(f):
   The entry of interactions dict collect "from" nodes.
   '''
   comps = dict();
+  kofe = {'KO':[], 'FE':[]};
   defInters = dict();
   optInters = dict();
 
-  for c in f.readline().strip().split(', '): # read the components line
-    comps[c[0]] = [int(i) for i in c[2:-1].split()]; # store allowed rules num
-  f.readline(); # skip the blank line
+  for c in f.readline().split(','): # read the components line
+    c = c.strip();
+    gene = c[:c.index('(')]; # the first word is the name
+    if(gene[-1] == '+'): 
+      gene = gene[:-1];
+      kofe['FE'].append(gene.strip('-'));
+    if(gene[-1] == '-'):
+      gene = gene[:-1];
+      kofe['KO'].append(gene.strip('+'));
+    rulelist = c[c.index('(')+1:c.index(')')];
+    comps[gene] = [int(i) for i in rulelist.split()]; # store allowed rules num
+    
   for l in f.readlines(): # loop every line for interaction
     l = l.strip().split();
+    if(not l): continue;
     if(l[-1] == 'optional'): addInter(optInters, l[0], l[1], l[2])
     else: addInter(defInters, l[0], l[1], l[2]); # defined interaction
 
-  return (comps, defInters, optInters);
+  return (comps, kofe, defInters, optInters);
 
 def addExp(d, name, t, stateList):
   appendEntry(d, name, (t, stateList));
@@ -59,16 +70,29 @@ def readExp(f):
 
   shortcut = '';
   for l in f.readlines():
-    l = l.strip().strip(';').split();
+    l = l.strip();
     if(not l): continue; # skip empty line
+
+    try: l = l[:l.index('"')] # remove commment
+    except ValueError: None;
+    try: l = l[:l.index(';')] # remove ;
+    except ValueError: None;
+
     if(shortcut): # inside the braket { }
       if(l[0] == '{'): continue # skip left bracket
       elif(l[0] == '}'): shortcut = '' # exit the braket;
       else:
-        addState(states, shortcut, l[0], int(l[2])); # record configuration
-    elif(l[0] == "//"): continue # comment line
+        (left, right) = l.split('=');
+        name = left.strip();
+        value = right.split()[0];
+        addState(states, shortcut, name, value); # record configuration
+    l = l.split();
+    if(l[0] == "//"): continue # comment line
     elif(l[0] == "under"): addExp(exps, l[1], int(l[3]), l[4:]) # recordexp
-    elif(l[0] == "let"): shortcut = l[1]; # ready to enter the braket
+    elif(l[0] == "let"):
+     shortcut = l[1]; # ready to enter the braket
+     try: shortcut = shortcut[:shortcut.index(':')]
+     except ValueError: None;
 
   return (exps, states);
 
@@ -134,25 +158,11 @@ def getFunction(comps):
   for c in product(*[rules[node] for node in nodes]):
     yield dict(zip(nodes, c));
 
-situ = ( lambda a, r: Or(a), # at leasr one activator. rep should be empty
-         lambda a, r: And(a), # all activator. rep should be empty
-
-         lambda a, r: And(Or(a), Not(Or(r))), # at least 1 act, and no rep
-         lambda a, r: And(And(a), Not(Or(r))), # all act, no rep
-         lambda a, r: And(Or(a), And(Or(r), Not(And(r)))), 
-         lambda a, r: And(Or(a), And(r)), # at least 1 act, and all rep
-         lambda a, r: And(And(a), Or(r)), # all act, and at least one rep
-         lambda a, r: And(*a + r), # all act and all rep
-
-         lambda a, r: Not(Or(r)), # no rep
-         lambda a, r: And(Or(r), Not(And(r))), );
-
-def _create_rule(num, act, rep, t = None):
-  if(not rep):
-    if(not act): return False # not activation
-    else: return (Or, And)[num % 2]; # no rep, return rule 0 or rule 1
+def _create_rule(num, act, rep, t):
+  if(not (rep or act)): return False; # no interactions
   postfix = t and ('_' + str(t)) or ''; # if t specified, append _t
   actt = [Bool(a + postfix) for a in act];
+  if(not rep): return (Or, And)[num % 2](actt); # only apply rule 1 or 2
   rept = [Bool(r + postfix) for r in rep];
   if(num == 0): return And(actt)
   elif(num == 1): return Or(actt)
@@ -168,18 +178,31 @@ def _create_rule(num, act, rep, t = None):
   elif(num == 16): return And(Or(rept), Not(And(rept)))
   elif(num == 17): return Not(And(rept));
 
-def _add_function(s, fnum, node, acrp, step = 20):
-  (ac, rp) = acrp;
-  for t in range(1, step): # 19 transitions from t-1 to t
-    s.add(Bool(node + '_' + str(t)) == 
-               _create_rule(fnum, ac, rp, t-1));
+def _with_KOFE(node, kofe):
+  if(node in kofe['KO']):
+    ko = Bool('KO_' + node);
+    if(node in kofe['FE']):
+      fe = Bool('FE_' + node);
+      return lambda x: Or(fe, And(Not(ko), x)) # both ko and fe
+    else: return lambda x: And(Not(ko), x) # only ko
+  if(node in kofe['FE']):
+    fe = Bool('FE_' + node);
+    return lambda x: Or(fe, x)
+  else: return lambda x: x; # no kofe
 
-def applyFunctions(s, funcd, sgraph, step = 20):
+def _add_function(s, fnum, node, kofe, acrp, step = 20):
+  (ac, rp) = acrp;
+  tune = _with_KOFE(node, kofe);
+  for t in range(1, step): # 19 transitions from t-1 to t
+    s.add(Bool(node + '_' + str(t)) == tune(
+               _create_rule(fnum, ac, rp, t-1)));
+
+def applyFunctions(s, funcd, kofe, sgraph, step = 20):
   '''
   Take the solver, function number, and a sorted graph.
   '''
   for node in funcd:
-    _add_function(s, funcd[node], node, sgraph[node], step);
+    _add_function(s, funcd[node], node, kofe, sgraph[node], step);
     
 def addConstrains(s, exp, states):
   '''
@@ -189,7 +212,9 @@ def addConstrains(s, exp, states):
   for (t, shortcuts) in exp:
     for sc in shortcuts:
       for (node, value) in states[sc]:
-        s.add(Bool(node + '_' + str(t)) == bool(value));
+        if(node.split('_')[0] in ('KO', 'FE')):
+          s.add(Bool(node) == bool(value))
+        else: s.add(Bool(node + '_' + str(t)) == bool(value));
 
 
 ### Output Utilities ###
